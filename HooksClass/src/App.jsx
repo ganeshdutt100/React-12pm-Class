cb8befb7a187c4ecaf955c6c502539d5cec61228
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-07 13:22:52 +0530

    Update dark mode

--- HooksClass/src/App.jsx
+++ HooksClass/src/App.jsx
@@ -1,20 +1,22 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css'
-import { File1 } from './components/File1'
-import { File2 } from './components/File2'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { File3 } from './components/File3';
-import { File4 } from './components/File4';
+import { Textfile } from './components/Textfile';
+// import { File1 } from './components/File1'
+// import { File2 } from './components/File2'
+// import { File3 } from './components/File3';
+// import { File4 } from './components/File4';
 
 function App() {
 
   return (
     <>  
-    <File1/>
+    <Textfile/>
+    {/* <File1/>
      <File2/>
      <File3/>
-     <File4/>
+     <File4/> */}
     </>
   )
 }
---
+++ HooksClass/src/components/Textfile.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export const Textfile = () => {
+    const [darkMode, setdarkMode] = useState(false);
+
+    const toggleDarkMode = () =>{
+        setdarkMode(!darkMode)
+       
+       
+    }
+  return (
+    <div>
+<div className="mb-3 container">
+  <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+
+ {/* (con) ? true : false */}
+
+  <textarea style={{background : darkMode ? 'black' :  'white'  ,  color : darkMode ? 'white': 'black'}} className="form-control" id="exampleFormControlTextarea1" rows={7} defaultValue={""} cols={5} value={darkMode ? 'Enabled dark mode' : 'Enabled light mode'} />
+
+  <button className="btn btn-danger mt-4" onClick={toggleDarkMode}>{darkMode ? 'Disable dark mode' : 'Enable Dark mode'}</button>
+</div>
+
+    </div>
+  )
+}
