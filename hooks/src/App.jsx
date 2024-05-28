785ea3724b816ed83901dec6c24c18c194eb2fb8
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-28 13:55:37 +0530

    Use Effect Example

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(60);
 
   // useEffect(() => {
   //   first;
@@ -19,16 +19,28 @@ function App() {
     console.log("i am Deepika.....!");
   }, []); // load
 
-  useEffect(() => {
-    alert(` current value  : ${count} `);
-  }, [count]);
+  // Dependencies Array
+  // useEffect(() => {
+  //   alert(` current value  : ${count} `);
+  // }, [count]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((x) => x - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <>
-      <h1>i am Ganesh Dutt</h1>
-      <button onClick={() => setCount(count + 1)}>ADD</button>
+      {/* <h1>i am Ganesh Dutt</h1> */}
+      {/* <button onClick={() => setCount(count + 1)}>ADD</button>
       <input type="text" value={count} size={4} />
-      <button onClick={() => setCount(count - 1)}>SUB</button>
+      <button onClick={() => setCount(count - 1)}>SUB</button> */}
+
+      <p>Seconds : {count}</p>
     </>
   );
 }
