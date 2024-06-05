bc9297e97ab93ec50a00bc550c039215b3ad05d7
Ganesh Dutt <gdutt2304@gmail.com>
2024-06-05 12:42:26 +0530

    Event update 1

--- events/src/components/Events1.jsx
+++ events/src/components/Events1.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Events1 = () => {
-  const handleClick = () => {
-    alert("Button clicked");
+  const [value, setValue] = useState("");
+  // const handleClick = () => {
+  //   alert("Button clicked");
+  // };
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Form Submitted With Value : " + value);
   };
   return (
-    <div>
-      <button onClick={handleClick}>Click me</button>
-    </div>
+    <>
+      <form onSubmit={handleSubmit} action="">
+        <input type="text" value={value} onChange={handleChange} />
+
+        <button type="submit">Click me</button>
+      </form>
+    </>
   );
 };
